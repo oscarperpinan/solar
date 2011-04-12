7ce262576f18a485b81707857d6f6af01cc9369c
oscarperpi <oscarperpi@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2011-04-12 12:43:55 +0000

    Meteo-class.Rd and readMAPA.Rd improved with Wh/m2; more changes in redEstaciones.R; new redGN.RData

--- drafts/redEstaciones.R
+++ drafts/redEstaciones.R
@@ -32,11 +32,11 @@ buscaGN <- function(x){
 ##busco en geonames para todas las estaciones contenidas en RedEstaciones
 redGN <- apply(RedEstaciones, 1, buscaGN)
 redGN <- do.call('rbind', redGN)
-##añado las variables NomProv y NomEst
-redGN$NomProv <- RedEstaciones$NomProv
-redGN$NomEst <- RedEstaciones$NomEst
+##añado las variables de RedEstaciones
+redGN <- cbind(redGN, RedEstaciones)
 ##Elimino aquellos registros para los que no encuentro resultados
 redGN <- redGN[!is.na(redGN$lat),]
+##save(file='redGN.RData', redGN)
 ##¿cuantas estaciones hemos localizado?
 nrow(redGN)
 ###Construyo un objeto Spatial usando las funcionalidades de sp, maptools, etc.
@@ -49,13 +49,43 @@ spRedGN <- SpatialPointsDataFrame(coords=redGN[c('lng', 'lat')],
 
 ##Descargo y descomprimo un zip de http://biogeo.ucdavis.edu/data/diva/adm/ESP_adm.zip
 ##Contiene un Shapefile con información de las fronteras entre provincias de españa.
+old <- getwd()
+setwd('/home/oscar/temp')##Cambiar!!!
 ##Leo el contenido:
 mapaSHP <- readShapeLines('ESP_adm/ESP_adm2.shp', proj4string=proj)
+setwd(old)
 ##y lo transformo en un objeto SpatialLines, con grosr de linea definido por lwd
 mapaES <- list('sp.lines', mapaSHP, lwd=0.5)
 ##Ya puedo representar los resultados:
-spplot(spRedGN['NomProv'], sp.layout=mapaES, scales=list(draw=TRUE), key.space='right')
+spplot(spRedGN['NomProv'], sp.layout=mapaES, scales=list(draw=TRUE), key.space='right', cex=0.5)
 
+
+##En la llamada a APPLYdebo eliminar las dos últimas columnas (caracteres) para que no convierta todo a character
+spainMeteo <- apply(redGN[, 1:4], 1,
+                      function(x)readMAPA(prov=x[3], est=x[4], start='01/01/2004', end='31/12/2010', lat=x[1])
+                      )
+##save(file='spainMeteo', spainMeteo)
+
+meanYearlySums <- function(x)mean(aggregate(getG0(x), year, sum, na.rm=1))
+
+spainG0y <- sapply(spainMeteo, meanYearlySums)/1000
+
+redGN$G0y=spainG0y
+spRedGN$G0y=spainG0y
+
+ncuts=15
+paleta=colorRampPalette(brewer.pal(9, 'Reds'))(ncuts)
+spplot(spRedGN['G0y'], sp.layout=mapaES, col.regions=paleta,
+       scales=list(draw=TRUE), ylim=c(35, 45), xlim=c(-10, 5),
+       cuts=ncuts, key.space='right', cex=0.8)
+
+vgmG0y <- variogram(G0y~1, data=spRedGN)
+plot(vgmG0y)
+fitvgmG0y <- fit.variogram(vgmG0y, vgm(20000, 'Sph', 10))
+
+krigeG0y <- krige(G0y~1, spRedGN, spRedGN, model=fitvgmG0y)
+
+################################################################################
 ###Otra alternativa. En lugar de buscar en geonames, uso la información del IGN
 ###http://wiki.openstreetmap.org/wiki/ES:NGBE
 ###Hay que descargar el NGBE.zip, que contiene una base de datos MDB.
