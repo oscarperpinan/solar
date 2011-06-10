146dfc37516d21f2a59b562fdfb80edd87e0bacc
oscarperpi <oscarperpi@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2011-06-10 23:22:51 +0000

    add identify method and choosePoints function for Raster

--- drafts/latticeRaster.R
+++ drafts/latticeRaster.R
@@ -1,3 +1,6 @@
+library(mgcv)
+library(hexbin)
+
 xscale <- function(...){ans <- xscale.components.default(...); ans$top=FALSE; ans}
 yscale <- function(...){ans <- yscale.components.default(...); ans$right=FALSE; ans}
 
@@ -139,3 +142,55 @@ setMethod('splom',
                   pscale=0, ...)
           }
           )
+setGeneric('identify', function(object, ...){standardGeneric('identify')})
+
+setMethod('identify', signature=(object='Raster'),
+          definition=function(object, label, column=1, row=1, pch=13, cex=0.6, col='darkgreen',...){
+            trellis.focus('panel', column, row, ...)
+            trellisType <- as.character(trellis.last.object()$call)[1]
+            if (trellisType=='splom'){
+              idx <- panel.link.splom(pch=pch, cex=cex, col=col,...)
+##              object[idx,]
+            } else {
+  ##            lbl=round(object@data[label], 1)
+              idx <- panel.identify(label='x', pch=pch, cex=cex, col=col,...)
+##              as.data.frame(object)[idx,]
+            }
+            trellis.unfocus()
+            idx
+          }
+          )
+
+
+choosePoints <- function(...){
+  trellis.focus('panel', 1, 1)
+  x <- trellis.panelArgs()$x
+  y <- trellis.panelArgs()$y
+  xy <- xy.coords(x, y, recycle = TRUE)
+  x <- xy$x
+  y <- xy$y
+  px <- convertX(unit(x, "native"), "points", TRUE)
+  py <- convertY(unit(y, "native"), "points", TRUE)
+  pointsData <- cbind(px, py)
+
+  border <- as.numeric()
+
+  while (TRUE){
+    ll <- grid.locator(unit='native')
+    if (!is.null(ll)){
+      lpoints(ll, col='black', cex=0.7, pch=3)
+      lx <- convertX(unit(ll$x, 'native'), 'points', FALSE)
+      ly <- convertY(unit(ll$y, 'native'), 'points', FALSE)
+      border <- rbind(border, c(lx, ly))
+    } else {    
+      break
+    }
+  }
+  inside <- in.out(border, pointsData)
+  dataInside <- data.frame(xin=x[inside], yin=y[inside])
+  drawLayer(layer(panel.points(xin, yin, col='black', cex=0.5),
+                  data=dataInside)
+            )
+  trellis.unfocus()
+  result <- inside
+}
