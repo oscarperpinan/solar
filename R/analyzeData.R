4e708f87d870cd7e824c930f578c40616d033162
Oscar Perpiñán <oscar.perpinan@gmail.com>
2016-04-16 13:52:04 +0200

    Mark TargetDiagram as defunct

--- R/analyzeData.R
+++ R/analyzeData.R
@@ -1,16 +1,3 @@
 analyzeData <- function(x, na.rm = TRUE){
-    .Deprecated("'tdr' package")
-    myStats <- function(x){
-        cbind(mean(x, na.rm = na.rm),
-              median(x, na.rm = na.rm),
-              sd(x, na.rm = na.rm),
-              mad(x, na.rm = na.rm),
-              IQR(x, na.rm = na.rm)
-              )
-    }
-
-    xStats <- apply(x, 1, myStats)
-    xStats <- zoo(t(xStats), index(x))
-    names(xStats) <- c('mean', 'median', 'sd', 'mad', 'IQR')
-    xStats
+    .Defunct("tdr package", package = "solaR")
 }
