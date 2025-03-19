6ffd74106f8e80ff24b2a7423418261d2765b878
Oscar Perpiñán Lamigueiro <oscar.perpinan@upm.es>
2025-03-19 12:17:32 +0100

    Move to Codeberg

--- R/TargetDiagram.R
+++
@@ -1,5 +0,0 @@
-TargetDiagram<-function(x, end, ndays, ref=NULL, color=NULL, cex=0.8, ...){
-    .Defunct(new = "tdr::targetDiagram", package = "solaR")
-    
-}
-
--- R/analyzeData.R
+++
@@ -1,3 +0,0 @@
-analyzeData <- function(x, na.rm = TRUE){
-    .Defunct("tdr package", package = "solaR")
-}
